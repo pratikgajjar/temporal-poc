4d118c7574f069eb5e1b28bca04053f15900433b
Pratik Gajjar <pratik@fampay.in>
2024-03-31 01:41:04 +0530

    Add sample golang worker, workflow and producer.
    Add temporal docker setup with postres and scylla backend

---
+++ starter/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"github.com/google/uuid"
+	flow "github.com/pratikgajjar/tempoc/workflow"
+	"go.temporal.io/sdk/client"
+	"log"
+)
+
+func main() {
+	// The client is a heavyweight object that should be created once per process.
+	c, err := client.Dial(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	// This workflow ID can be user business logic identifier as well.
+	workflowID := "cron_" + uuid.New().String()
+	workflowOptions := client.StartWorkflowOptions{
+		ID:           workflowID,
+		TaskQueue:    "cron",
+		CronSchedule: "* * * * *",
+	}
+
+	we, err := c.ExecuteWorkflow(context.Background(), workflowOptions, flow.SampleCronWorkflow)
+	if err != nil {
+		log.Fatalln("Unable to execute workflow", err)
+	}
+	log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
+}
